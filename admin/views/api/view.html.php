068dd7f7f1285c823352ac2f541b960d541c1b52
pangkhon <pang.khon@ezpizee.com>
2021-01-05 10:17:42 +0700

    fixed request form data

--- admin/views/api/view.html.php
+++ admin/views/api/view.html.php
@@ -148,7 +148,7 @@ class EzpzViewApi extends HtmlView
             if (isset($this->contentType) && $this->contentType === 'application/json') {
                 $response = $this->client->post($this->uri, $this->body);
             }
-            else if (isset($this->contentType) && strpos($this->contentType, 'multipart/form-data;') !== false) {
+            else if (isset($this->contentType) && strpos($this->contentType, 'multipart/form-data') !== false) {
                 if ($this->hasFileUploaded()) {
                     $response = $this->submitFormDataWithFile();
                 }
