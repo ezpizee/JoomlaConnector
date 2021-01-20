9b2c70fd9b5ffa5ede3c60aab3ee555cf2a434e6
Sothea Nim <sothea.nim@ezpizee.com>
2021-01-19 21:26:08 -0500

    updated the library

--- src/com_ezpz/site/ezpz.php
+++ src/com_ezpz/site/ezpz.php
@@ -1,21 +1,36 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_ezpz
+ * @subpackage  com_gx2cms
  *
- * @copyright   Copyright (C) 2020 - 2021 Ezpizee Co., Ltd. All rights reserved.
+ * @copyright   Copyright (C) 2018 - 2021 WEBCONSOL Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
-// Get an instance of the controller prefixed by Ezpz
-$controller = JControllerLegacy::getInstance('Ezpz');
+if (!defined('EZPIZEE_DS')) {
+    define('EZPIZEE_DS', DIRECTORY_SEPARATOR);
+    define('EZPIZEE_COMP_ROOT', __DIR__);
+}
 
-// Perform the Request task
-$input = JFactory::getApplication()->input;
-$controller->execute($input->getCmd('task'));
+if (!file_exists(JPATH_LIBRARIES.EZPIZEE_DS.'ezpzlib'.EZPIZEE_DS.'autoload.php')) {
+    include JPATH_COMPONENT_ADMINISTRATOR.EZPIZEE_DS.'asset'.EZPIZEE_DS.'html'.EZPIZEE_DS.'install-instructions.php';
+}
+else {
+    include_once JPATH_LIBRARIES.EZPIZEE_DS.'ezpzlib'.EZPIZEE_DS.'autoload.php';
+    \Ezpizee\ContextProcessor\CustomLoader::appendPackage([
+        'EzpizeeJoomla' => JPATH_COMPONENT_ADMINISTRATOR.EZPIZEE_DS.'lib'.EZPIZEE_DS.'src'
+    ], true);
 
-// Redirect if set by the controller
-$controller->redirect();
+    // Get an instance of the controller prefixed by GX2CMS
+    $controller = JControllerLegacy::getInstance('Ezpz');
+
+    // Perform the Request task
+    $input = JFactory::getApplication()->input;
+    $controller->execute($input->getCmd('task'));
+
+    // Redirect if set by the controller
+    $controller->redirect();
+}
